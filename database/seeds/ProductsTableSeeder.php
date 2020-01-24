16cf847c2882f433aad8fee16c3a8a9659bbc8b4
Zorca Orcinus <vs@zorca.org>
2020-01-24 10:28:53 +0300

    Products seeder fixed

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -11,28 +12,34 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('products')->insert(
+        Product::insert(
             [
                 [
                     'name' => 'Pepperoni',
                     'type' => 'pizza',
                     'description' => 'Slices of crispy pepperoni & creamy mozzarella with fresh tomatoes.',
                     'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
-                    'price' => 10.40
+                    'price' => 10.40,
+                    'created_at' => now(),
+                    'updated_at' => now(),
                 ],
                 [
                     'name' => 'Margherita',
                     'type' => 'pizza',
                     'description' => 'Diced tomatoes & stretchy mozzarella, topped with oregano.',
                     'properties' => 'Mozarella, Tomatoes, Oregano',
-                    'price' => 8.60
+                    'price' => 8.60,
+                    'created_at' => now(),
+                    'updated_at' => now(),
                 ],
                 [
                     'name' => 'Texas',
                     'type' => 'pizza',
                     'description' => 'Legendary Texas pizza with bavarian sausages and mushrooms.',
                     'properties' => 'Corn, Onion, Mushrooms, Bavarian sausages, Mozarella, BBQ sauce',
-                    'price' => 13.90
+                    'price' => 13.90,
+                    'created_at' => now(),
+                    'updated_at' => now(),
                 ]
             ]
         );
