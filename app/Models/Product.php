f9909be71cdfe160f574a5529f90c561898eff21
Zorca Orcinus <vs@zorca.org>
2020-01-24 21:34:24 +0300

    Price casting for decimal value

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'type', 'description', 'properties'];
+
+    protected $casts = [
+        'price' => 'float',
+    ];
 }
