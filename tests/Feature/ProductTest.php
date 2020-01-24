43caf1630706988a54d7adb8f9e98fb99e1f11a6
Zorca Orcinus <vs@zorca.org>
2020-01-24 22:46:30 +0300

    Update method test added

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -30,6 +31,30 @@ class ProductTest extends TestCase
                 'price' => 10.10,
                 'created_at' => $time,
                 'updated_at' => $time,
+            ]);
+    }
+
+    public function testsProductsAreUpdatedCorrectly()
+    {
+        $product = Product::create([
+            'name' => 'Test Pizza',
+            'type' => 'pizza',
+            'description' => 'Slices of tasty pepperoni & creamy mozzarella.',
+            'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
+            'price' => 10.10,
+        ]);
+
+        $payload = [
+            'name' => 'Another Salad',
+            'type' => 'salad',
+        ];
+
+        $response = $this->json('PUT', '/api/products/' . $product->id, $payload)
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => 4,
+                'name' => 'Another Salad',
+                'type' => 'salad',
             ]);
     }
 }
