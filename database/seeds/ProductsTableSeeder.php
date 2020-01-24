428ba3d7adc60a6f0ca8bed2df860ad511334030
Zorca Orcinus <vs@zorca.org>
2020-01-24 09:41:11 +0300

    Seeder fix

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,19 +17,22 @@ class ProductsTableSeeder extends Seeder
                     'name' => 'Pepperoni',
                     'type' => 'pizza',
                     'description' => 'Slices of crispy pepperoni & creamy mozzarella with fresh tomatoes.',
-                    'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce'
+                    'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
+                    'price' => 10.40
                 ],
                 [
                     'name' => 'Margherita',
                     'type' => 'pizza',
                     'description' => 'Diced tomatoes & stretchy mozzarella, topped with oregano.',
                     'properties' => 'Mozarella, Tomatoes, Oregano',
+                    'price' => 8.60
                 ],
                 [
                     'name' => 'Texas',
                     'type' => 'pizza',
                     'description' => 'Legendary Texas pizza with bavarian sausages and mushrooms.',
                     'properties' => 'Corn, Onion, Mushrooms, Bavarian sausages, Mozarella, BBQ sauce',
+                    'price' => 13.90
                 ]
             ]
         );
