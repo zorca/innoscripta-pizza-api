07e29806d6cc1835d781a73f39887fefa6c9b848
Zorca Orcinus <vs@zorca.org>
2020-01-24 23:07:20 +0300

    Test for all products list

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -70,6 +71,69 @@ class ProductTest extends TestCase
 
         $this->json('DELETE', '/api/products/' . $product->id, [])
             ->assertStatus(204);
+    }
+
+    public function testProductsAreListedCorrectly()
+    {
+
+        Product::create([
+            'name' => 'Test Pizza',
+            'type' => 'pizza',
+            'description' => 'Slices of tasty pepperoni & creamy mozzarella.',
+            'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
+            'price' => 10.10,
+        ]);
+
+        Product::create([
+            'name' => 'Another Pizza',
+            'type' => 'pizza',
+            'description' => 'Another pizza description.',
+            'properties' => 'Mozarella, Tomatoes',
+            'price' => 18.25,
+        ]);
+
+        $response = $this->json('GET', '/api/products', [])
+            ->assertStatus(200)
+            ->assertJson([
+                [
+                    'name' => 'Pepperoni',
+                    'type' => 'pizza',
+                    'description' => 'Slices of crispy pepperoni & creamy mozzarella with fresh tomatoes.',
+                    'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
+                    'price' => 10.40,
+                ],
+                [
+                    'name' => 'Margherita',
+                    'type' => 'pizza',
+                    'description' => 'Diced tomatoes & stretchy mozzarella, topped with oregano.',
+                    'properties' => 'Mozarella, Tomatoes, Oregano',
+                    'price' => 8.60,
+                ],
+                [
+                    'name' => 'Texas',
+                    'type' => 'pizza',
+                    'description' => 'Legendary Texas pizza with bavarian sausages and mushrooms.',
+                    'properties' => 'Corn, Onion, Mushrooms, Bavarian sausages, Mozarella, BBQ sauce',
+                    'price' => 13.90,
+                ],
+                [
+                    'name' => 'Test Pizza',
+                    'type' => 'pizza',
+                    'description' => 'Slices of tasty pepperoni & creamy mozzarella.',
+                    'properties' => 'Mozarella, Pepperoni, Tomatoes, BBQ sauce',
+                    'price' => 10.10,
+                ],
+                [
+                    'name' => 'Another Pizza',
+                    'type' => 'pizza',
+                    'description' => 'Another pizza description.',
+                    'properties' => 'Mozarella, Tomatoes',
+                    'price' => 18.25,
+                ]
+            ])
+            ->assertJsonStructure([
+                '*' => ['id', 'name', 'type', 'description', 'properties', 'price', 'created_at', 'updated_at'],
+            ]);
     }
 }
 
